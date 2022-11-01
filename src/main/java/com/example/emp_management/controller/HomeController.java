9e28dc5de7c998deac8dfb13921505e8de3cb28c
Alamin <alamin8740@gmail.com>
2022-11-01 18:01:13 +0600

    Registration Successfully added inside database

--- src/main/java/com/example/emp_management/controller/HomeController.java
+++ src/main/java/com/example/emp_management/controller/HomeController.java
@@ -1,11 +1,21 @@
 package com.example.emp_management.controller;
 
+import com.example.emp_management.model.EmpRegistration;
+import com.example.emp_management.service.EmpRegistrationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+    private final EmpRegistrationService empRegistrationService;
+
+    public HomeController(EmpRegistrationService empRegistrationService) {
+        this.empRegistrationService = empRegistrationService;
+    }
+
     @GetMapping("/")
     public String showHome(Model model){
         return "index";
@@ -13,7 +23,16 @@ public class HomeController {
 
     @GetMapping("/reg_form")
     public String loginPage(Model model){
+        EmpRegistration empRegistration = new EmpRegistration();
+        model.addAttribute("empRegistration",empRegistration);
         return "reg_form";
+    }
+
+    @PostMapping("/store_emp_reg")
+    public String saveEmp(Model model, @ModelAttribute EmpRegistration empRegistration){
+        empRegistrationService.saveEmpRegInfo(empRegistration);
+        model.addAttribute("empRegistration",empRegistration);
+        return "index";
     }
 
 }
